7f46149430ea1140b967ef97178e17c407429f72
ebsonjoy <ebsonjoy721@gmail.com>
2025-01-03 02:19:06 +0530

    init cmt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3001,
     proxy:{
       '/api':{
-        target:'https://apis.coupidscourt.site',
+        target:'https://api.coupidscourt.site',
         changeOrigin:true
       }
     }
